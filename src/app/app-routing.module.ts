64da4e11127e6cd87b231d43002cc99b814df37f
maheshgaddam2410 <mahesh.gaddam@peopletech.com>
2023-04-26 23:38:23 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
-import {LoginComponent} from "./components/login/login.component";
-import {RegisterComponent} from "./components/register/register.component";
 import {CartComponent} from "./components/cart/cart.component";
 import {CheckoutComponent} from "./components/checkout/checkout.component";
 import {ProductComponent} from "./components/product/product.component";
@@ -13,15 +11,9 @@ const routes: Routes = [
   {
     path: '', component: HomeComponent
   },
-  // {
-  //   path: 'login', component: LoginComponent
-  // },
-  // {
-  //   path: 'register', component: RegisterComponent
-  // },
-  // {
-  //   path: 'product/:id', component: ProductComponent
-  // },
+  {
+    path: 'product/:id', component: ProductComponent
+  },
   {
     path: 'cart', component: CartComponent
   },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {HomeComponent} from './components/home/home.component';
-import {LoginComponent} from './components/login/login.component';
-import {RegisterComponent} from './components/register/register.component';
 import {CartComponent} from './components/cart/cart.component';
 import {CheckoutComponent} from './components/checkout/checkout.component';
 import {HttpClientModule} from "@angular/common/http";
@@ -24,8 +21,6 @@ import {ToastrModule} from 'ngx-toastr';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent,
-    RegisterComponent,
     CartComponent,
     CheckoutComponent,
     ProductComponent,
